5da82562359ef6d983fe99a450aaa09ff5ff7561
Himangshu <hksarkar@outlook.com>
2025-04-12 10:39:19 +0530

    Query for product with reviews

--- resolvers/Product.js
+++ resolvers/Product.js
@@ -3,6 +3,11 @@ const Product = {
     const { categoryId } = parent;
     return db.categories.find((category) => category.id === categoryId);
   },
+  reviews: (parent, orgs, { db }) => {
+    const { id } = parent;
+    console.log(parent);
+    return db.reviews.filter((review) => review.productId === id);
+  },
 };
 
 module.exports = { Product };
